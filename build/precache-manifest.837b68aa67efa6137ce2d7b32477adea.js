110291b62b33259a68d806728721926c183e87ef
jaredami <jarstorm@imail.iu.edu>
2019-01-12 13:26:44 -0500

    removed build folder from .gitignore

---
+++ build/precache-manifest.837b68aa67efa6137ce2d7b32477adea.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a246e40c7ba3d76441702f52576832c0",
+    "url": "/static/media/green-circle.a246e40c.png"
+  },
+  {
+    "revision": "482df35adbc0cf5e23ab",
+    "url": "/static/css/main.e620c3e6.chunk.css"
+  },
+  {
+    "revision": "7fd17b8a9eca23e9db04",
+    "url": "/static/js/1.7fd17b8a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c7e2c051e2df4798b871cd5d684c5123",
+    "url": "/static/media/beige-circle.c7e2c051.png"
+  },
+  {
+    "revision": "f8510e6163af96dd3909e1c9d64296b7",
+    "url": "/static/media/blue-circle.f8510e61.png"
+  },
+  {
+    "revision": "482df35adbc0cf5e23ab",
+    "url": "/static/js/main.482df35a.chunk.js"
+  },
+  {
+    "revision": "6624eacbf16269d9c3c8c21772d4e527",
+    "url": "/static/media/orange-circle.6624eacb.png"
+  },
+  {
+    "revision": "47d3ae1630a80f38e5d752e2b8f605a5",
+    "url": "/static/media/purple-circle.47d3ae16.png"
+  },
+  {
+    "revision": "76abbd8d5ea418e06aa7bbd650ed93f3",
+    "url": "/static/media/red-circle.76abbd8d.png"
+  },
+  {
+    "revision": "6024db70f1d5d49d3eabe7e87bd26978",
+    "url": "/static/media/turquoise-circle.6024db70.png"
+  },
+  {
+    "revision": "8c743b4a82fd40f7f96ec37e5cac829e",
+    "url": "/static/media/yellow-circle.8c743b4a.png"
+  },
+  {
+    "revision": "4e24d6e37b63e858b059675ccb373b0b",
+    "url": "/index.html"
+  }
+];
